884bbfc7729bb3dba3905b934a4fc6a066feb5cb
Mamleev Muhammad <muhammad.mamleev.1998@gmail.com>
2024-01-07 17:12:03 +0500

    Добавлена функция рестарта, и управление прыжком

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 
 
 import Phaser from 'phaser';
+import {validate} from "@babel/core/lib/config/validation/options";
 
 
 const config = {
@@ -10,23 +11,58 @@ const config = {
     height: 600,
     physics: {
         // Arcade physics plugin
-        default: 'arcade'
+        default: 'arcade',
+        arcade: {
+            debug: true,
+            gravity: {y: 300},
+        },
     },
     scene: {
         preload,
         create,
+        update,
     }
 }
+
+
+let bird = null;
+const flapVelocity = 200;
+const initialBirdPosition = {
+    x: config.width * 0.1,
+    y: config.height / 2,
+}
+
 
 // Loading assets (images, music, animations ...)
 function preload() {
     // 'this' context - scene
-    debugger
+    this.load.image('sky', 'assets/sky.png');
+    this.load.image('bird', 'assets/bird.png');
 }
 
 function  create() {
-    debugger
+    // x, y, key of the image
+    this.add.image(0, 0, 'sky').setOrigin(0);
+    bird = this.physics.add.sprite(initialBirdPosition.x, initialBirdPosition.y, 'bird').setOrigin(0);
+
+    this.input.on('pointerdown', flap)
+    this.input.keyboard.on('keydown_SPACE', flap)
+}
+
+function update(time, delta) {
+    if (bird.y > config.height - bird.height || bird.y < 0) {
+        restartBirdPosition();
+    }
+}
+
+function restartBirdPosition() {
+    bird.x = initialBirdPosition.x;
+    bird.y = initialBirdPosition.y;
+    bird.body.velocity.y = 0;
+}
+
+function flap() {
+    bird.body.velocity.y = -flapVelocity;
 }
 
 new Phaser.Game(config);
-
