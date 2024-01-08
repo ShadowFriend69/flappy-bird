98bf5d07d3073ad04dd68c9209c8252a0e4615ca
Mamleev Muhammad <muhammad.mamleev.1998@gmail.com>
2024-01-08 18:02:54 +0500

    Поправлено взаимодйствие игрока с препятсвиями

--- src/scenes/PlayScene.js
+++ src/scenes/PlayScene.js
@@ -32,6 +32,7 @@ class PlayScene extends Phaser.Scene {
         this.createBG();
         this.createBird();
         this.createPipes();
+        this.createColliders();
         this.handleInputs();
     }
 
@@ -45,7 +46,8 @@ class PlayScene extends Phaser.Scene {
     }
 
     createBird() {
-        this.bird = this.physics.add.sprite(this.config.startPosition.x, this.config.startPosition.y, 'bird').setOrigin(0);
+        this.bird = this.physics.add.sprite(this.config.startPosition.x, this.config.startPosition.y, 'bird')
+            .setOrigin(0);
         this.bird.body.gravity.y = 300;
     }
 
@@ -53,13 +55,21 @@ class PlayScene extends Phaser.Scene {
         this.pipes = this.physics.add.group();
 
         for (let i = 0; i < PIPES_TO_RENDER; i++) {
-            const upperPipe = this.pipes.create(0, 0, 'pipe').setOrigin(0, 1);
-            const lowerPipe = this.pipes.create(0, 0, 'pipe').setOrigin(0, 0);
+            const upperPipe = this.pipes.create(0, 0, 'pipe')
+                .setImmovable(true)
+                .setOrigin(0, 1);
+            const lowerPipe = this.pipes.create(0, 0, 'pipe')
+                .setImmovable(true)
+                .setOrigin(0, 0);
 
             this.placePipe(upperPipe, lowerPipe);
         }
 
         this.pipes.setVelocityX(-200);
+    }
+
+    createColliders() {
+        this.physics.add.collider(this.bird, this.pipes, this.gameOver, null, this);
     }
 
     handleInputs() {
@@ -69,7 +79,7 @@ class PlayScene extends Phaser.Scene {
 
     checkGameStatus() {
         if (this.bird.y > this.config.height - this.bird.height || this.bird.y < 0) {
-            this.restartBirdPosition();
+            this.gameOver();
         }
     }
 
@@ -108,10 +118,12 @@ class PlayScene extends Phaser.Scene {
         return rightMostX
     }
 
-    restartBirdPosition() {
-        this.bird.x = this.config.startPosition.x;
-        this.bird.y = this.config.startPosition.y;
-        this.bird.body.velocity.y = 0;
+    gameOver() {
+        // this.bird.x = this.config.startPosition.x;
+        // this.bird.y = this.config.startPosition.y;
+        // this.bird.body.velocity.y = 0;
+        this.physics.pause();
+        this.bird.setTint(0xEE4824);
     }
 
     flap() {
