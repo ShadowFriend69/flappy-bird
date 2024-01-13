41c526dc4ead994293b901206134e752534a485b
Mamleev Muhammad <muhammad.mamleev.1998@gmail.com>
2024-01-13 16:59:49 +0500

    Добавлена кнопка паузы и метод паузы сцены и физики

--- src/scenes/PlayScene.js
+++ src/scenes/PlayScene.js
@@ -29,6 +29,7 @@ class PlayScene extends Phaser.Scene {
         this.load.image('sky', 'assets/sky.png');
         this.load.image('bird', 'assets/bird.png');
         this.load.image('pipe', 'assets/pipe.png');
+        this.load.image('pause', 'assets/pause.png');
     }
 
     create() {
@@ -37,6 +38,7 @@ class PlayScene extends Phaser.Scene {
         this.createPipes();
         this.createColliders();
         this.createScore();
+        this.createPauseBtn();
         this.handleInputs();
     }
 
@@ -83,6 +85,18 @@ class PlayScene extends Phaser.Scene {
         this.scoreText = this.add.text(16, 16, `Score: ${0}`, {fontSize: '32px', fill: '#000'});
         this.bestScoreText = this.add.text(16, 52, `Best score: ${bestScore || 0}`, {fontSize: '18px', fill: '#000'});
 
+    }
+
+    createPauseBtn() {
+        const pauseButton = this.add.image(this.config.width - 10, this.config.height - 10, 'pause')
+            .setInteractive()
+            .setScale(2)
+            .setOrigin(1);
+
+        pauseButton.on('pointerdown', () => {
+            this.physics.pause();
+            this.scene.pause();
+        })
     }
 
     handleInputs() {
