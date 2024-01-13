72d0ecaddd4ec010e3d27b69092cc167bf36c9bb
Mamleev Muhammad <muhammad.mamleev.1998@gmail.com>
2024-01-13 19:23:30 +0500

    Добавлены пукты меню

--- src/scenes/BaseScene.js
+++ src/scenes/BaseScene.js
@@ -8,7 +8,17 @@ class BaseScene extends Phaser.Scene {
     constructor(key, config) {
         super(key);
         this.config = config;
-
+        this.screenCenter = [
+            config.width / 2,
+            config.height / 2,
+        ];
+        this.fontSize = '32px';
+        this.fontFill = '#CD00FF';
+        this.fontOptions = {
+            fontSize: this.fontSize,
+            fill: this.fontFill,
+        };
+        this.linehight = 42;
     }
 
     create() {
@@ -17,6 +27,18 @@ class BaseScene extends Phaser.Scene {
 
     createBG() {
         this.add.image(0, 0, 'sky').setOrigin(0);
+    }
+
+    createMenu(menu) {
+        let lastMenuPositionY = 0;
+
+        menu.forEach(menuItem => {
+            const menuPosition = [this.screenCenter[0], this.screenCenter[1] + lastMenuPositionY];
+            this.add.text(...menuPosition, menuItem.text, this.fontOptions)
+                .setOrigin(0.5, 1);
+            lastMenuPositionY += this.linehight;
+        })
+
     }
 
 }
