a3d37d76b8b34357e13d685f551a3dcbc632268e
Mamleev Muhammad <muhammad.mamleev.1998@gmail.com>
2024-01-13 18:45:25 +0500

    Добавлены сцены MenuScene.js и PreloadScene.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@
 
 import Phaser from 'phaser';
 import PlayScene from "./scenes/PlayScene";
+import MenuScene from "./scenes/MenuScene";
+import PreloadScene from "./scenes/PreloadScene";
 
 const WIDTH = 800;
 const HEIGHT = 600;
@@ -16,6 +18,15 @@ const SHARED_CONFIG = {
     startPosition: BIRD_POSITION,
 }
 
+const Scenes = [
+    PreloadScene,
+    MenuScene,
+    PlayScene,
+]
+
+const createScene = Scene => new Scene(SHARED_CONFIG)
+const initScenes = () => Scenes.map(createScene)
+
 const config = {
     // WebGl (Web graphics library) JS Api for rendering 2D and 3D graphics
     type: Phaser.AUTO,
@@ -27,7 +38,7 @@ const config = {
             debug: true,
         },
     },
-    scene: [new PlayScene(SHARED_CONFIG)]
+    scene: initScenes()
 }
 
 new Phaser.Game(config);
---
+++ src/scenes/MenuScene.js
@@ -0,0 +1,24 @@
+
+
+import Phaser from "phaser";
+
+
+class MenuScene extends Phaser.Scene {
+
+    constructor(config) {
+        super('MenuScene');
+        this.config = config;
+
+    }
+
+    create() {
+        this.createBG();
+    }
+
+    createBG() {
+        this.add.image(0, 0, 'sky').setOrigin(0);
+    }
+
+}
+
+export default MenuScene;
